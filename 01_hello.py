cfef0fdb4ef5b93ea500ff09dadcc87eddf88db9
AYUSH KUMAR <57504080+ayush26kr@users.noreply.github.com>
2021-06-21 21:20:45 +0530

    Add files via upload

---
+++ 01_hello.py
@@ -0,0 +1,8 @@
+#comment
+import os
+print("hello world....")
+#printing a multi line string in python
+print('''Twinkle twinkle little star 
+how i wonder what you are
+up above the world so high
+like a diamond in the sky''')
