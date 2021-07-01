e07c7583b108eab6f3f943f0bbe16276cbe6fe42
AYUSH KUMAR <57504080+ayush26kr@users.noreply.github.com>
2021-07-01 12:12:35 +0530

    Add files via upload

---
+++ average_of_two_numbers_by_user.py
@@ -0,0 +1,3 @@
+a=int(input("Enter value of a: "))
+b=int(input("Enter value of b: "))
+print("Average of ",a,"and ",b,"is : ",(a+b)/2)
---
+++ printing_table.py
@@ -0,0 +1,5 @@
+a=int(input("Enter number: "))
+i=1
+while i<=10:
+    print(a,"x",i,"=",(i*a))
+    i+=1
