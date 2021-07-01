48f047e254a6ddf88c5b145c33f522ebe2cd2fb4
AYUSH KUMAR <57504080+ayush26kr@users.noreply.github.com>
2021-07-01 12:27:40 +0530

    Add files via upload

---
+++ slicing_string.py
@@ -0,0 +1,8 @@
+name="Ayush"
+print(name[1:3]) #1 included 3 not included
+print(name[-3:-1])
+
+#slicing with skip values
+name="AyushIsAGoodBoy"
+d=name[::2]  #same as [0:end:2] from 0 : to end of string : skip every 2 element  
+print(d)
